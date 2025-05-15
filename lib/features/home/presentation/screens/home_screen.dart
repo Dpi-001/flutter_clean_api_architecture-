2a68fc003a1bd5e6ebb45b6166c295bfcad817c5
Dpi-001 <sudip9845197918@gmail.com>
2025-05-15 12:38:19 +0545

    Bloc:Builder

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_3/features/home/presentation/blocs/bloc/todo_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Todo')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Todo')),
+      body: BlocBuilder<TodoBloc, TodoState>(
+        builder: (context, state) {
+          if (state is FetchTodoFail) {
+            return Center(child: Text(state.errorMessage));
+          } else if (state is FetchTodoLoaded) {
+            return Center(child: Text("${state.todo.length}"));
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
   }
 }
